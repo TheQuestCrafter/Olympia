261bfdaadfb26961bc9488ed61e84f7f43b4dc19
CalvinOh <45076299+CalvinOh@users.noreply.github.com>
2019-01-16 03:25:26 -0600

    Fixed Zeus for vertical shooting

--- Assets/Scripts/BulletManager.cs
+++ Assets/Scripts/BulletManager.cs
@@ -81,33 +81,59 @@ public class BulletManager : MonoBehaviour
                 fireRate = 0.7f;
                 if (Time.time > nextFire)
                 {
-                  
-
-                    float angleStep = 15f;
-                    float angle = 75f;
-
-                    for (int i = 0; i <= numberofProjectiles - 1; i++)
+                    if (vertical)
                     {
+                        float angleStep = -15f;
+                        float angle = 15f;
 
-                        float projectileDirXposition = bulletSpawn.position.x + Mathf.Sin((angle * Mathf.PI) / 180) * radius;
-                        float projectileDirYposition = bulletSpawn.position.y + Mathf.Cos((angle * Mathf.PI) / 180) * radius;
 
-                        Vector3 projectileVector = new Vector3(projectileDirXposition, projectileDirYposition);
-                        Vector3 projectileMoveDirection = (projectileVector - bulletSpawn.position).normalized * movespeed;
+                        for (int i = 0; i <= numberofProjectiles - 1; i++)
+                        {
 
-                        var proj = Instantiate(zeusBulletPrefab , bulletSpawn.position, Quaternion.identity);
-                        proj.GetComponent<Rigidbody2D>().velocity =
-                            new Vector2(projectileMoveDirection.x, projectileMoveDirection.y);
+                            float projectileDirXposition = bulletSpawn.position.x + Mathf.Sin((angle * Mathf.PI) / 180) * radius;
+                            float projectileDirYposition = bulletSpawn.position.y + Mathf.Cos((angle * Mathf.PI) / 180) * radius;
 
-                        angle += angleStep;
-                        Destroy(proj, bulletLife);
+                            Vector3 projectileVector = new Vector3(projectileDirXposition, projectileDirYposition);
+                            Vector3 projectileMoveDirection = (projectileVector - bulletSpawn.position).normalized * movespeed;
+
+                            var proj = Instantiate(zeusBulletPrefab, bulletSpawn.position, Quaternion.identity);
+                            proj.GetComponent<Rigidbody2D>().velocity =
+                                new Vector2(projectileMoveDirection.x, projectileMoveDirection.y);
+
+                            angle += angleStep;
+                            Destroy(proj, bulletLife);
+
+                            nextFire = Time.time + fireRate;
+                        }
+
+
                     }
+                    else
+                    {
+                        float angleStep = 15f;
+                        float angle = 75f;
 
-                    nextFire = Time.time + fireRate;
+                        for (int i = 0; i <= numberofProjectiles - 1; i++)
+                        {
 
+                            float projectileDirXposition = bulletSpawn.position.x + Mathf.Sin((angle * Mathf.PI) / 180) * radius;
+                            float projectileDirYposition = bulletSpawn.position.y + Mathf.Cos((angle * Mathf.PI) / 180) * radius;
 
+                            Vector3 projectileVector = new Vector3(projectileDirXposition, projectileDirYposition);
+                            Vector3 projectileMoveDirection = (projectileVector - bulletSpawn.position).normalized * movespeed;
 
+                            var proj = Instantiate(zeusBulletPrefab, bulletSpawn.position, Quaternion.identity);
+                            proj.GetComponent<Rigidbody2D>().velocity =
+                                new Vector2(projectileMoveDirection.x, projectileMoveDirection.y);
+
+                            angle += angleStep;
+                            Destroy(proj, bulletLife);
+                        }
+
+                        nextFire = Time.time + fireRate;
+                    }
                 }
+               
                 break;
             case 2:
                 weaponDamage = 0.2f;
