aa62785016d957cfcd01477f51506611b34d5500
CalvinOh <45076299+CalvinOh@users.noreply.github.com>
2019-01-16 19:06:13 -0600

    Poseidon Push

---
+++ Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour {
+    [SerializeField]
+    private float speed;
+    private Vector3 startPOS;
+
+	// Use this for initialization
+	void Start () {
+        startPOS = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate((new Vector3(-1, 0, 0)) * speed * Time.deltaTime);
+
+        if (transform.position.x < -6.4)
+        {
+            transform.position = startPOS;
+        }
+    }
+}
