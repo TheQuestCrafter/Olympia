9bad509c9502dc286b28aa3d5ae5f53c04596360
ekarlson238 <43217591+ekarlson238@users.noreply.github.com>
2019-01-19 17:25:04 -0600

    Added fade in to Zeus Minion fight

---
+++ Assets/Scripts/FadeIn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeIn : MonoBehaviour {
+
+    [SerializeField]
+    private Image image;
+
+    private Color tempColor;
+	// Use this for initialization
+	void Start () {
+        tempColor = image.color;
+        tempColor.a = 1f;
+        image.color = tempColor;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (tempColor.a >= 0f)
+        {
+            tempColor.a -= 0.2f * Time.deltaTime;
+        }
+        
+        image.color = tempColor;
+    }
+}
