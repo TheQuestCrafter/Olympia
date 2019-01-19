51522fb6f4882aa3df2796af8d2c5045d70b8f3c
CalvinOh <45076299+CalvinOh@users.noreply.github.com>
2019-01-18 20:21:26 -0600

    ZeusBigBomb Damage Player

--- Assets/Scripts/ZeusBigBombScript.cs
+++ Assets/Scripts/ZeusBigBombScript.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class ZeusBigBombScript : MonoBehaviour {
 
+    GameObject player;
 
 	// Use this for initialization
 	void Awake ()
     {
-        
+        if (this.player == null)
+        {
+            PlayerBehavior temp = FindObjectOfType<PlayerBehavior>();
+            this.player = temp.gameObject;
+        }
     }
 	
 	// Update is called once per frame
@@ -17,5 +22,10 @@ public class ZeusBigBombScript : MonoBehaviour {
        
             Destroy(this.gameObject, 3f);
     
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        player.GetComponent<PlayerBehavior>().health--;
     }
 }
