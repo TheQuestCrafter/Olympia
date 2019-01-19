638a775bd79f90c0e810cc35d07f04fca7852f30
TheQuestCrafter <thequestcrafter@gmail.com>
2019-01-18 19:58:10 -0600

    Player Health UI & More
    -Player Health UI added
    -Fixed visual error with hearts and Chronos
    -All scenes added to the scene index in play order.
    -Added player health ui and added the boss health component to each to each scene with a boss.

---
+++ Assets/Scripts/HealthUI.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUI : MonoBehaviour
+{
+    GameObject player;
+    [SerializeField]
+    Image heart1;
+    [SerializeField]
+    Image heart2;
+    [SerializeField]
+    Image heart3;
+	// Use this for initialization
+	void Start () {
+        if (this.player == null)
+        {
+            PlayerBehavior temp = FindObjectOfType<PlayerBehavior>();
+            this.player = temp.gameObject;
+        }
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        if (player == null)
+        {
+            heart1.gameObject.SetActive(false);
+        }
+        else if (player.GetComponent<PlayerBehavior>().health == 1)
+        {
+            heart2.gameObject.SetActive(false);
+        }
+        else if (player.GetComponent<PlayerBehavior>().health == 2)
+        {
+            heart3.gameObject.SetActive(false);
+        }
+        else if (player.GetComponent<PlayerBehavior>().health == 3)
+        {
+            heart1.gameObject.SetActive(true);
+            heart2.gameObject.SetActive(true);
+            heart3.gameObject.SetActive(true);
+        }
+	}
+}
