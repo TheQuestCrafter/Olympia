fe3b56a9dc46fb501a9f26762c1f58ab5a308bc4
TheQuestCrafter <thequestcrafter@gmail.com>
2019-01-19 20:31:35 -0600

    Screen Sizer for Title Screen

---
+++ Assets/Scripts/ScreenSizer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenSizer : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake() {
+        Screen.SetResolution(1200, 800, false);
+	}
+}
